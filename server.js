e8ea7f94a4148f3ab0ba71b20d36e6db534ef584
Shafiul islam mashruf <bemashrufislam@gmail.com>
2026-05-23 14:05:51 +0600

    build to dist

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.post('/api/recommend', async (req, res) => {
 });
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'client/build'))); 
+app.use(express.static(path.join(__dirname, 'client/dist'))); 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
